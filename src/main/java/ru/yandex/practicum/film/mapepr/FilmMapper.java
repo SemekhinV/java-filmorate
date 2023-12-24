7f5feadf399f2077f307f33c42d933b198764da4
Semekhin Vasiliy <72353030+SemekhinV@users.noreply.github.com>
2023-12-24 19:23:43 +0300

    fix docker build error

--- src/main/java/ru/yandex/practicum/film/mapepr/FilmMapper.java
+++ src/main/java/ru/yandex/practicum/film/mapepr/FilmMapper.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.film.mapepr;
 
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.film.dto.FilmDto;
 import ru.yandex.practicum.film.entity.Film;
 
 import java.util.List;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface FilmMapper {
 
--- src/main/java/ru/yandex/practicum/user/mapepr/UserMapper.java
+++ src/main/java/ru/yandex/practicum/user/mapepr/UserMapper.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.user.mapepr;
 
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.user.dto.UserDto;
 import ru.yandex.practicum.user.entity.User;
 
 import java.util.List;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
